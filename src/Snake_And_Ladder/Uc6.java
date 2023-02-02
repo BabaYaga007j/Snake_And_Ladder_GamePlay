1fa3293e49fcf23312529296babdd62d8f7e94ab
BabaYaga007j <soumyasourav35@gmail.com>
2023-02-02 17:37:10 +0530

    Win the game and position

---
+++ src/Snake_And_Ladder/Uc6.java
@@ -0,0 +1,13 @@
+package Snake_And_Ladder;
+
+public class Uc6 {
+	
+	public static void main(String[] args) {
+		
+		Uc5.main(args);
+		System.out.println("Totla dice rolled "+Uc5.count_of_dice);
+		
+		
+	}
+
+}
