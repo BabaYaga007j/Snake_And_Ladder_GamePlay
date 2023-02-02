75eacafd599adf746a723caaeece106a72dbeacd
BabaYaga007j <soumyasourav35@gmail.com>
2023-02-02 17:40:12 +0530

    Welcome to snake and ladder game

---
+++ src/Snake_And_Ladder/WelcomeNote.java
@@ -0,0 +1,20 @@
+package Snake_And_Ladder;
+
+
+
+public class WelcomeNote {
+
+	/*Snake & Ladder
+
+	Simulator
+
+	This problem simulates a Snake and Ladder Game. The Player
+	starts from 0 rolls the die to get a number between 1 to 6, finds
+	a safe place, ladder or a snake keeps doing till the winning spot
+
+	100 is achieved.*/
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to snake lader game problem");
+	}
+}
