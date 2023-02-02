a53a6f3759a7eb6a1ebb412997731fd5a97d72d4
BabaYaga007j <soumyasourav35@gmail.com>
2023-02-02 17:33:39 +0530

    Winning position 100

---
+++ src/Snake_And_Ladder/Uc4.java
@@ -0,0 +1,76 @@
+package Snake_And_Ladder;
+
+public class Uc4 {
+	
+	static int count_of_dice = 0;
+	static int gamePlay() 
+	{
+		int position = 0;
+		
+		int condition = 10;
+
+		while (condition > 0)
+		{
+			int options = (int) (Math.random() * 3 + 1);
+			System.out.println("option is " + options);
+			int dice_roll = (int) (Math.random() * 6 + 1);
+			System.out.println("Dice Rolled " + dice_roll);
+			
+
+			if (dice_roll > 0)
+			{
+				count_of_dice++;
+			}
+
+			if (options == 1)
+			{
+				System.out.println("No Play");
+				position = position;
+			} else if (options == 2) 
+			{
+				System.out.println("there is ladder");
+				position += dice_roll;
+			} else if (options == 3) 
+			{
+				System.err.println("there is Snake");
+				position -= dice_roll;
+				if (position < 0)
+				{
+					position = 0;
+				} else {
+					position = position;
+				}
+			} else 
+			{
+				System.out.println("roll dice again");
+			}
+
+			System.out.println("Position " + position);
+			int Previous_position = position;
+			
+
+			if (position > 100) 
+			{
+				position = Previous_position;
+				
+			}
+
+			System.out.println("-------------------------");
+
+			if (position == 100)
+			{
+				System.out.println("Player is winner");
+				break;
+			}
+			else 
+			{
+				System.out.println("roll a dice again");
+			}
+		}
+		return position;
+	}
+	public static void main(String[] args) {
+		gamePlay();
+		
+	}
+}
