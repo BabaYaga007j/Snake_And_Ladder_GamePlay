ed637235ac1e1745ce6f9664a5c169e63cdbf3b5
BabaYaga007j <soumyasourav35@gmail.com>
2023-02-02 17:22:16 +0530

    Starting Position Zero

---
+++ src/Snake_And_Ladder/Uc1.java
@@ -0,0 +1,15 @@
+package Snake_And_Ladder;
+
+/*Snake and Ladder
+game played with
+single player at start
+position 0*/
+public class Uc1 {
+	
+	static int dice1=0;
+	
+	public static void main(String[] args) {
+		System.out.println("Snake and Lader game played by a single player");
+		System.out.println("Sarts with position 0");
+	}
+}
